0442435b403968146573f24b0239591eff958d05
Gavin Carew <gjcarew@gmail.com>
2022-07-12 20:51:16 -0700

    Update potluck_spec with menu and category methods

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -18,4 +18,6 @@ class Potluck
     end
   end
 
+  
+
 end
--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -42,4 +42,43 @@ RSpec.describe Potluck do
     expect(@potluck.get_all_from_category(:appetizer).first).to be(couscous_salad)
     expect(@potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
   end
+
+  it 'can display a menu as a hash' do
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+
+    @potluck.add_dish(couscous_salad)
+    @potluck.add_dish(summer_pizza)
+    @potluck.add_dish(roast_pork)
+    @potluck.add_dish(cocktail_meatballs)
+    @potluck.add_dish(candy_salad)
+    @potluck.add_dish(bean_dip)
+
+    expect(@potluck.menu).to eq({:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],
+                                :entres=>["Cocktail Meatballs", "Roast Pork"],
+                                :desserts=>["Candy Salad"]})
+  end
+
+  it 'can tell the ratio of a category to the other categories' do
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+
+    @potluck.add_dish(couscous_salad)
+    @potluck.add_dish(summer_pizza)
+    @potluck.add_dish(roast_pork)
+    @potluck.add_dish(cocktail_meatballs)
+    @potluck.add_dish(candy_salad)
+    @potluck.add_dish(bean_dip)
+
+    expect(@potluck.ratio(:appetizer)).to eq(50.0)
+  end
+
 end
